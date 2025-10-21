b673295aeb576e8610ee43284a1a8b97debcf0c7
IT24100855 <it24100855@my.sliit.lk>
2025-10-22 00:32:26 +0530

    Added Payment Tracking

---
+++ src/main/java/backend/property_sales_system/propetease_payment_tracking/PaymentTrackingApplication.java
@@ -0,0 +1,11 @@
+package backend.property_sales_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTrackingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentTrackingApplication.class, args);
+    }
+}
